5c70d08e7c150272a394a778117e653135ee806c
Jordan Herbert <jbherbwebdesign@gmail.com>
2019-05-13 14:10:45 -0700

    Add files via upload

---
+++ radio.js
@@ -0,0 +1,52 @@
+let radio;
+function theradio(){
+radio = createRadio();
+radio.position(0,0);
+	radio.class("bdd");
+radio.option('Millenium Falcon',1);
+radio.option('Enterprise',2);
+radio.option('Outer Space',3);
+radio.option('The Ocean',4);
+radio.style(width,'60px');
+}
+
+function drawradio(){
+var val = radio.value();
+ switch(val){
+ 	case '1': mill();break;
+ 	case '2': enter();break;
+ 	case '3': space();break;
+ 	case '4': matthew();break;
+ }
+}
+let background1,background2,background3,background4,anch2,anch3,anch4;
+
+function mill(){
+let background1=createImg('spac.jpg');
+background1.size(windowWidth,windowHeight);
+background1.position(0,0);
+
+setup();
+}
+function enter(){
+let background2=createImg('enterprise.jpg');
+background2.size(windowWidth,windowHeight);
+background2.position(0,0);
+
+setup();
+}
+function space(){
+let background3=createImg('universe.jpg');
+background3.size(windowWidth,windowHeight);
+background3.position(0,0);
+
+setup();
+}
+function matthew(){
+let background4=createImg('mattt.jpg');
+background4.size(windowWidth,windowHeight);
+background4.position(0,0);
+setup();
+}
+
+
